851163e09f7a78d2bad8f7c006295515f359d8ec
Felipe Serna <feserna86@gmail.com>
2021-10-10 22:27:32 -0500

    Hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,9 @@
 /* This is a simple Java program.
  * FileName: "HelloWorld.java". */
-class HelloWorld
-{
+public class HelloWorld {
 	// Your program begins with a call to main().
 	// Prints "Hello, World" to the terminal window.
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		System.out.println("Hello, World. I am Felipe Serna");
 	}
 }
