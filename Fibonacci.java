542a0d15ceb9f06c348bf98a0b4e8fb3b73adf0b
Felipe Serna <feserna86@gmail.com>
2021-10-13 08:16:37 -0500

    All the Fibonacci sequence

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,17 +1,32 @@
 // Fibonacci using recursion
 public class Fibonacci {
     public static void main(String[] args) {
-        int result = fibonacci(10);
-        // 34
-        System.out.println(result);
+
+        int number = 13; // Fibonacci sequence until this term
+
+        for (int i = 1; i  <= number; i++) {
+            
+            int result = fibonacci(i);
+            
+            if (i != number) {
+                System.out.print(result + ", ");
+            }
+            
+            else {
+                System.out.println(result);
+            }
+        }        
     }
     public static int fibonacci(int k) {
+        
         if (k == 1) {
             return 0;
         }
+        
         else if (k == 2) {
             return 1;
         }
+        
         else {
             return fibonacci(k - 1) + fibonacci(k - 2);
         }
