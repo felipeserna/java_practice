e559a2de0bdfa3aca35b400cf40b038b47686bbd
Felipe Serna <feserna86@gmail.com>
2021-10-21 18:40:06 -0500

    Accepted solution

---
+++ 69_square_root/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution a = new Solution();
+        System.out.println(a.mySqrt(4));
+        System.out.println(a.mySqrt(8));
+    }
+}
---
+++ 69_square_root/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int mySqrt(int x) {
+        int i = (int)Math.sqrt(x);
+        return i;
+    }
+}
